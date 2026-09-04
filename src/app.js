037290e019d3090f03f94e35138950807f5db889
Leo Milberg <leo@Leos-MacBook-Air-2.local>
2026-09-03 20:50:27 -0400

    static working

--- src/app.js
+++ src/app.js
@@ -5,20 +5,21 @@ const path = require('path');
 const fs = require('fs');
 
 const PORT = 8000;
-const DROP_DIR = path.join(__dirname, '../public/games');
+const DROP_DIR = path.join(__dirname, '../static/games');
 
 const app = express();
 
 app.use(express.json());
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../public'))
+app.use(express.static(path.join(__dirname, '../static')))
 
-app.get('/', (req, res) => {
-	res.render('views/upload');
-});
 
-app.get('/arcade', (req, res) => {
-	res.render('views/arcade');
-});
+
+// app.get('/', (req, res) => {
+// 	res.render('views/upload', { title: 'Upload - Scratch Arcade' });
+// });
+
+// app.get('/arcade', (req, res) => {
+// 	res.render('views/arcade', { title: 'Scratch Arcade' });
+// });
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`));
